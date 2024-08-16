1d9fe5d1be9cd30054e2c2613edc51680f313744
ihrskyfish <ihr17867950748@126.com>
2024-08-16 09:05:15 +0800

    写了一点 开发辅助工具的程序

--- just_try.sh
+++ just_try.sh
@@ -6,8 +6,9 @@ SSH_IP="192.168.0.102"
 SSH_PORT="8022"
 
 BASIC_REMOTE_PATH="/sdcard/脚本/remote"
-REMOTE_FOLDER="test"
+
 DIRNAME=$(basename "$PWD")
+REMOTE_FOLDER=${DIRNAME}
 
 TARGET_DIR=${BASIC_REMOTE_PATH}/${REMOTE_FOLDER}
 
@@ -15,11 +16,14 @@ TARGET_DIR=${BASIC_REMOTE_PATH}/${REMOTE_FOLDER}
 
 
 
+ssh -p ${PORT} ${SSH_IP}   <<EOF
+rm -rf ${TARGET_DIR}
+EOF
 
 scp -p $PORT  -r  ./      ${USER}@${SSH_IP}:${TARGET_DIR}
 
 ssh -p ${PORT} ${SSH_IP}   <<EOF
-adb shell am start -n org.autojs.autoxjs.v6/org.autojs.autojs.external.shortcut.ShortcutActivity -a android.intent.action.MAIN -e path " /sdcard/脚本/${DIRNAME}/main.js "
+adb shell am start -n org.autojs.autoxjs.v6/org.autojs.autojs.external.shortcut.ShortcutActivity -a android.intent.action.MAIN -e path " ${TARGET_DIR}/main.js "
 EOF
 
 
